b9a6e68c473052f23f7d68dfabfdc54aa08269e9
Suyu Xiao <1203111345@qq.com>
2017-11-06 17:12:34 +0800

    update

--- Analysis/Physics/Chic2invi/Chic2invi/Chic2invi-00-00-01/cmt/cleanup.sh
+++
@@ -1,9 +0,0 @@
-if test "${CMTROOT}" = ""; then
-  CMTROOT=/afs/ihep.ac.cn/bes3/offline/ExternalLib/contrib/CMT/v1r20p20081118; export CMTROOT
-fi
-. ${CMTROOT}/mgr/setup.sh
-tempfile=`${CMTROOT}/mgr/cmt -quiet build temporary_name`
-if test ! $? = 0 ; then tempfile=/tmp/cmt.$$; fi
-${CMTROOT}/mgr/cmt cleanup -sh -pack=Chic2invi -version=Chic2invi-00-00-01 -path=/besfs/groups/nphy/users/xiaosy/bes/chic2invi/v0.1/analysis/physics/psip $* >${tempfile}; . ${tempfile}
-/bin/rm -f ${tempfile}
-
