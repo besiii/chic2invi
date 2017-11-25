72609a70151a8bbf380b1775599ca1e354654311
Suyu Xiao <1203111345@qq.com>
2017-11-25 10:20:00 +0800

    update mc12 and data3650

--- TestRelease/TestRelease-00-00-84/cmt/setup.sh
+++ TestRelease/TestRelease-00-00-84/cmt/setup.sh
@@ -1,4 +1,4 @@
-# echo "Setting Jpsi2invi Jpsi2invi-00-00-01 in /afs/ihep.ac.cn/users/x/xiaosy/bes/jpsi2invi/v0.1/Analysis/Physics/PsiPrime"
+# echo "Setting TestRelease TestRelease-00-00-84 in /afs/ihep.ac.cn/users/x/xiaosy/bes/chic2invi/v0.1"
 
 if test "${CMTROOT}" = ""; then
   CMTROOT=/afs/ihep.ac.cn/bes3/offline/ExternalLib/contrib/CMT/v1r20p20081118; export CMTROOT
@@ -7,6 +7,6 @@ fi
 
 tempfile=`${CMTROOT}/mgr/cmt -quiet build temporary_name`
 if test ! $? = 0 ; then tempfile=/tmp/cmt.$$; fi
-${CMTROOT}/mgr/cmt setup -sh -pack=TestRelease -version=TestRelease-00-00-84 -path=/afs/ihep.ac.cn/users/x/xiaosy/bes/jpsi2invi/v0.1  -no_cleanup $* >${tempfile}; . ${tempfile}
+${CMTROOT}/mgr/cmt setup -sh -pack=TestRelease -version=TestRelease-00-00-84 -path=/afs/ihep.ac.cn/users/x/xiaosy/bes/chic2invi/v0.1  -no_cleanup $* >${tempfile}; . ${tempfile}
 /bin/rm -f ${tempfile}
 
