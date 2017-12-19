ebbf04dad481e196b8c61f1ee6927d84d96f859b
Suyu Xiao <1203111345@qq.com>
2017-12-19 16:24:55 +0800

    update

--- submit.sh
+++ submit.sh
@@ -14,14 +14,14 @@ usage() {
     printf "\n\t%-9s  %-40s"  "0.1.1"    "Split MC sample with each group 20G"
     printf "\n\t%-9s  %-40s"  "0.1.2"    "Submit Condor jobs on MC"
     printf "\n\t%-9s  %-40s"  "0.1.3"    "Check Condor jobs on MC."
-	printf "\n\t%-9s  %-40s"  "0.1.4"    "Merge root file on MC."
+    printf "\n\t%-9s  %-40s"  "0.1.4"    "Merge root file on MC."
     printf "\n\t%-9s  %-40s"  "0.1.5"   "Select events on MC."
     printf "\n\t%-9s  %-40s"  ""         ""
     printf "\n\t%-9s  %-40s"  "0.2"      "[run on data3650]"
     printf "\n\t%-9s  %-40s"  "0.2.1"    "Split con3650 data with each group 20G"
     printf "\n\t%-9s  %-40s"  "0.2.2"    "Submit Condor jobs on con3650 data"     
     printf "\n\t%-9s  %-40s"  "0.2.3"    "Check Condor jobs on con3650 data"     
-	printf "\n\t%-9s  %-40s"  "0.2.4"    "Merge root file on con3650 data"     
+    printf "\n\t%-9s  %-40s"  "0.2.4"    "Merge root file on con3650 data"     
     printf "\n\t%-9s  %-40s"  "0.2.5"    "Select events on con3650 data"     
     printf "\n\n" 
 }
@@ -108,163 +108,6 @@ case $option in
     0.2.5) echo  "Select event on con3650 data..."
 	   mkdir run/analysis
 	   root analysis.cxx
-	   ;; 
-
-    0.1.16) echo "Split MC09 sample with each group 20G ..."
-	   ./python/get_samples.py /bes3fs/offline/MC/664-1/psip/dst $HOME/bes/chic2invi/v0.1/run/samples/MC_664-1_psip.txt 20G
-	   # made 314 groups 
-	   ;;
-    0.1.17) echo "Submit Condor jobs on 09 MC..."
-	    mkdir run/MC09
-	    mkdir -p run/log/MC09  
-	    qsub pbs/qsub_chic2invi_MC09.sh  
-	    ;;
-
-    0.1.18) echo "Check Condor jobs on 09 MC..."
-	   ./python/chk_pbsjobs.py run/MC09  314 
-	   ;;
-
-    0.1.19) echo  "Event selection on 09 MC..."
-	   mkdir run/events/MC09 
-	   ./python/sel_events.py  run/MC09/chic2invi_MC09-1.root  run/events/MC09/chic2invi_MC09-1.root 
-	   ;; 
-
-    0.1.20) echo "Condor jobs for event selection on 09 MC..."
-	   mkdir run/events/MC09
-	   mkdir run/log/events/MC09   
-	   qsub pbs/qsub_chic2invi_events_MC09.sh  
-	   ;;
-
-    0.1.21) echo "Check Condor jobs of event selection on 09 MC..."
-	   ./python/chk_pbsjobs.py run/events/MC09  314
-	   ;;
-
-    0.1.22) echo  "Merge event files on 09 MC..."
-	   mkdir run/hist/MC09
-	   ./python/mrg_rootfiles.py  run/events/MC09 run/hist/MC09 
-	   ;; 
-
-    0.1.23) echo  "Plot summary with 09 MC..."
-	    ./python/plt_summary.py MC09  
-	    ;; 
-
-    0.1.24) echo "Run with Jpsi2incl on a few MC09 events ..."
-	   boss.exe pbs/jobOptions_chic2incl.txt
-	   ;;
-
-    0.1.25) echo "Submit Jpsi2incl Condor jobs on MC09..."
-	   mkdir -p run/chic2incl/MC09
-	   mkdir -p run/chic2incl/log/MC09
-	   qsub pbs/qsub_chic2incl_MC09.sh  
-	   ;;
-
-    0.1.26) echo "Check Jpsi2incl Condor jobs on 09 MC..."
-	   ./python/chk_pbsjobs.py run/chic2incl/MC09  314 
-	   ;;
-    
-    0.1.27) echo "Select Jpsi2incl events on MC09"
-	    mkdir -p run/chic2incl/events/MC09 
-	    ./python/sel_events.py  run/chic2incl/MC09/chic2incl_MC09-1.root  run/chic2incl/events/MC09/chic2incl_MC09-1.root 
-	   ;; 
-
-    # --------------------------------------------------------------------------
-    #  0.2 MC Sample 
-    # --------------------------------------------------------------------------
-    
-    0.2) echo "Running on MC sample..."
-	 ;;
-
-    0.2.1) echo "Run with a few events ..."
-	   boss.exe pbs/jobOptions_chic2invi.txt
-	   ;;
-    0.2.2) echo "Split psi(2S) MC sample with each group 20G ..."
-	   ./python/get_samples.py  /bes3fs/offline/MC/664p03/psip/12mc/dst $HOME/bes/chic2invi/v0.1/run/samples/mc_664p03_psip_12mc.txt 20G
-	   # made 394 groups 
-	   ;;
-
-    0.2.3) echo "Submit Condor jobs on psi(2S) MC sample..."
-	   mkdir -p run/chic2invi/job_text/mc12
-	   mkdir -p run/chic2invi/rootfile_mc12
-	   cd run/chic2invi/gen_script
-	   ./make_jobOption_file_mc12_chic2invi.sh
-	   cd ../job_text/mc12
-	   mv jobOptions_chic2invi_psip_mc12-394.txt jobOptions_chic2invi_psip_mc12-0.txt
-	   boss.condor -g physics -n 394 jobOptions_chic2invi_psip_mc12-%{ProcId}.txt 
-	   cd 
-	   cd bes/chic2invi/v0.1
-	   ;;
-
-    0.2.4) echo "Check Condor jobs on psi(2S) MC sample..."
-	   ./python/chk_condorjobs.py $HOME/bes/chic2invi/v0.1/run/chic2invi/rootfile_mc12  394 
-	   ;;
-
-    0.2.5) echo  "Select events on psi(2S) MC sample..."
-	   mkdir run/chic2invi/event_mc12 
-	   ./python/sel_events.py  run/chic2invi/rootfile_mc12/chic2invi_psip_mc12-1.root  run/chic2invi/event_mc12/chic2invi_psip_mc12-1.root 
-	   ;; 
-
-    0.2.6) echo "Submit selection PBS jobs on psi(2S) sample..."
-	   mkdir run/chic2invi/event_mc12
-	   mkdir run/chic2invi/job_text/mc12_event  
-	   cd run/chic2invi/gen_script
-	   ./make_jobOption_file_mc12_event.sh
-	   cd ../job_text/mc12_event
-	   mv jobOptions_chic2invi_psip_mc12_event-394.sh jobOptions_chic2invi_psip_mc12_event-0.sh
-	   chmod 755 jobOptions_chic2invi_psip_mc12_event-*
-	   hep_sub -g physics -n 394 jobOptions_chic2invi_psip_mc12_event-%{ProcId}.sh
-	   ;;
-
-    0.2.7) echo "Check events selection jobs on psi(2S) MC sample..."
-	   ./python/chk_condorjobs.py run/chic2invi/event_mc12  394 
-	   ;;
-
-    0.2.8) echo  "Merge events files..."
-	   mkdir run/chic2invi/hist_mc12
-	   ./python/mrg_rootfiles.py  run/chic2invi/event_mc12 run/chic2invi/hist_mc12
-	   ;; 
-
-    0.2.9) echo  "Plot summary with MC and psip(2S) MC ..."
-	   ./python/plt_summary.py hist_mc12
-	   ;; 
-
-    0.2.10) echo "Run with a few events on 09 MC ..." 
-	   boss.exe pbs/jobOptions_chic2invi.txt 
-	   ;;
-
-    0.2.11) echo "Split 09 psi(2S) MC sample with each group 20G ..."
-	   ./python/get_samples.py  /bes3fs/offline/MC/664p01/psip/mc/09dst $HOME/bes/chic2invi/v0.1/run/samples/mc_664p01_psip_09mc.txt 20G
-	   # made 106 groups 
-	   ;;
-
-    0.2.12) echo "Submit PBS jobs on 09 psi(2S) MC sample..."
-	    mkdir run/mc_psip09
-	    mkdir run/log/mc_psip09
-	    qsub pbs/qsub_chic2invi_mc_psip09.sh  
-	    ;;
-
-    0.2.13) echo "Check PBS jobs on 09 psi(2S) MC sample..."
-	   ./python/chk_pbsjobs.py $HOME/bes/chic2invi/v0.1/run/mc_psip09  106
-	   ;;
-
-    0.2.14) echo  "Event selection on 09 psi(2S) MC sample..."
-	    mkdir run/events/mc_psip09 
-	    ./python/sel_events.py  run/mc_psip09/chic2invi_mc_psip_09mc-1.root  run/events/mc_psip09/chic2invi_mc_psip_09mc-1.root 
-	    ;; 
-
-    0.2.15) echo "Submit selection PBS jobs on 09 psi(2S) sample..."
-	   mkdir run/events/mc_psip09
-	   mkdir run/log/events/mc_psip09
-	   qsub pbs/qsub_chic2invi_events_mc_psip09.sh  
-	   ;;
-    0.2.16) echo "Check selection jobs on 09 psi(2S) MC sample..."
-	   ./python/chk_pbsjobs.py run/events/mc_psip09  106 
-	   ;;
-    0.2.17) echo  "Merge events files of 09 psi(2S) MC sample..."
-	   mkdir run/hist/mc_psip09
-	   ./python/mrg_rootfiles.py  run/events/mc_psip09 run/hist/mc_psip09 
-	   ;; 
-    0.2.18) echo  "Plot summary with 09 MC and 09 psip(2S) MC ..."
-	   ./python/plt_summary.py MC09 mc_psip09
 	   ;; 
 
 esac
