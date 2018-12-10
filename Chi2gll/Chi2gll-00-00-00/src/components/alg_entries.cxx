5cd0046d2ed904ff075263feac03e1caa872ac1e
liukaihep <helloliukai@126.com>
2018-12-10 19:33:59 +0800

    first version chi2gll
    save basic information of lepton, photon, truth

---
+++ Chi2gll/Chi2gll-00-00-00/src/components/alg_entries.cxx
@@ -0,0 +1,10 @@
+#include "GaudiKernel/DeclareFactoryEntries.h"
+#include "Chi2gll/Chi2gll.h"
+
+DECLARE_ALGORITHM_FACTORY( Chi2gll )
+
+DECLARE_FACTORY_ENTRIES( Chi2gll ) {
+  DECLARE_ALGORITHM(Chi2gll)
+}
+
+
