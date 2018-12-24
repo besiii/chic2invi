d214022ce2f0fbb3df4b0284315d58c63f3df65e
amitraahul <amitraahul@lxslc611.ihep.ac.cn>
2018-12-24 11:56:17 +0800

    added progress bar and time duration

--- python/sel_events_chi2gll.py
+++ python/sel_events_chi2gll.py
@@ -7,8 +7,11 @@ Event Selection
 __author__="AMIT PATHAK <amit@ihep.ac.cn>"
 __copyright__="Copyright (c) AMIT PATHAK"
 __created__="[2018-12-17]"
-
+import sys
 import ROOT
+from progressbar import Bar, Percentage, ProgressBar
+from time import time
+from tools import duration
 
 #global histogram
 h_run = ROOT.TH1D('h_run','run',100, -27120, -25330)
@@ -21,13 +24,19 @@ def main ():
     outfile = 'dat/chi2gll_selection_001.root'
     fout = ROOT.TFile(outfile,"RECREATE")
     entries = t.GetEntriesFast()
+    pbar = ProgressBar(widgets=[Percentage(), Bar()], maxval=entries).start()
+    time_start = time()
+
     for k in range(entries):
+        pbar.update(k+1)
         t.GetEntry(k)
         h_run.Fill(t.run)
     
     h_run.Write()
     fout.Close()
-    print('finish')
+    pbar.finish()
+    dur = duration(time()-time_start)
+    sys.stdout.write(' \nDone in %s. \n' % dur)
 
 if __name__ =='__main__':
     main()
