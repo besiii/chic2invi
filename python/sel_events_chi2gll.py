fcfaab38ea212e1e6212aa557d9b0e255a6f42b3
amitraahul <amitraahul@lxslc614.ihep.ac.cn>
2019-02-21 11:05:41 +0800

    Updated selection code with some preliminary cuts

--- python/sel_events_chi2gll.py
+++ python/sel_events_chi2gll.py
@@ -33,13 +33,17 @@ h_inv_mass_electron12 = ROOT.TH1D('h_inv_mass_electron12', 'mass_electron12(GeV)
 h_transverse_momentum1 = ROOT.TH1D('h_transverse_momentum1', 'transverse_momentum1',100, -1, 2)
 h_transverse_momentum2 = ROOT.TH1D('h_transverse_momentum2', 'transverse_momentum2',100, -1, 2)
 h_transverse_momentum12 = ROOT.TH1D('h_transverse_momentum12', 'transverse_momentum12',100, -1, 2)
-h_inv_mass_gam1 = ROOT.TH1D ('h_rec_mass_gam1','rec_mass_gam1',100, 0, 3.7)
-h_inv_mass_gam2 = ROOT.TH1D ('h_rec_mass_gam2','rec_mass_gam2',80, 3.3, 3.7)
+h_rec_mass_gam1 = ROOT.TH1D ('h_rec_mass_gam1','rec_mass_gam1',100, 0, 3.8)
+h_rec_mass_gam2 = ROOT.TH1D ('h_rec_mass_gam2','rec_mass_gam2',80, 3.3, 3.7)
 h_inv_mass_chicj1 = ROOT.TH1D ('h_inv_mass_chicj1','inv_mass_chicj1',70, 3, 3.7)
 h_inv_mass_chicj2 = ROOT.TH1D ('h_inv_mass_chicj2','inv_mass_chicj2',100, 0,3.7)
 h_rec_mass_gam12 = ROOT.TH1D('h_rec_mass_gam12', 'rec_mass_gam12',50, -1.0, 4)
 h_energy_gamma1 = ROOT.TH1D('h_energy_gamma1', 'energy_gamma1', 100, 0, 2)
 h_energy_gamma2 = ROOT.TH1D('h_energy_gamma2', 'energy_gamma2', 100, 0, 1)
+
+# Global items
+p4trk = ROOT.vector('double')()
+p4shw = ROOT.vector('double')()
 
 def main ():
 
@@ -56,8 +60,12 @@ def main ():
     #infile = 'dat.bak/chi2gll_gen_mc.root'
     fin = ROOT.TFile(infile)
     t = fin.Get('ana')
+    t.SetBranchAddress("p4trk", p4trk)
+    t.SetBranchAddress("p4shw", p4shw)
+
     #outfile = 'dat.bak/chi2gll_selection_001.root'
     fout = ROOT.TFile(outfile,"RECREATE")
+    t_out = ROOT.TTree('ana', ana)
     entries = t.GetEntriesFast()
     pbar = ProgressBar(widgets=[Percentage(), Bar()], maxval=entries).start()
     time_start = time()
@@ -133,8 +141,8 @@ def main ():
         p4_shw_gam1 = cms_p4 - p4shw_gam1
         p4_shw_gam2 = cms_p4 - p4shw_gam2
 
-        inv_mass_gam1 = p4_shw_gam1.M()
-        inv_mass_gam2 = p4_shw_gam2.M()
+        rec_mass_gam1 = p4_shw_gam1.M()
+        rec_mass_gam2 = p4_shw_gam2.M()
         #print(inv_mass_gam1)
         #print(inv_mass_gam2)
         #exit()
@@ -145,8 +153,8 @@ def main ():
 
         rec_mass_gam12 = rec_gams_p4_raw.M()
         
-        h_inv_mass_gam1.Fill(inv_mass_gam1)
-        h_inv_mass_gam2.Fill(inv_mass_gam2)
+        h_rec_mass_gam1.Fill(rec_mass_gam1)
+        h_rec_mass_gam2.Fill(rec_mass_gam2)
         h_rec_mass_gam12.Fill(rec_mass_gam12)
 
         #print(p4shw_gam1, p4shw_gam1[3])
@@ -168,8 +176,8 @@ def main ():
     h_transverse_momentum1.Write()
     h_transverse_momentum2.Write()
     h_transverse_momentum12.Write()
-    h_inv_mass_gam1.Write()
-    h_inv_mass_gam2.Write()
+    h_rec_mass_gam1.Write()
+    h_rec_mass_gam2.Write()
     h_inv_mass_chicj1.Write()
     h_inv_mass_chicj2.Write()
     h_rec_mass_gam12.Write()
@@ -183,3 +191,4 @@ def main ():
 
 if __name__ =='__main__':
     main()
+    
